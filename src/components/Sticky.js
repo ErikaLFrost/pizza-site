cb8af83a63a5ed6363d323aaa2553f1915dee345
Bikelund <nicholas.barklund@eliassoftware.com>
2021-02-20 09:45:34 +0100

    About us and down arrow added

--- src/components/Sticky.js
+++ src/components/Sticky.js
@@ -12,19 +12,19 @@ const StickyMenu = styled.div`
     background-color: #A7A7A7;
     z-index: 2;
     margin: 0;
-`;
 
-const StickyText = styled.p`
-    margin: 0;
-    text-align: center;
-    padding: 6px 0;
-    font-family: 'Menlo';
-    font-size: 20px;
+    p{
+        margin: 0;
+        text-align: center;
+        padding: 6px 0;
+        font-family: 'Menlo';
+        font-size: 20px;
+    }
 `;
 
 const Sticky = () => {
     return (<StickyMenu>
-                <StickyText>Ring oss - 070 123 45 67</StickyText>
+                <p>Take Away - 070 123 45 67</p>
             </StickyMenu>)
 }
 
