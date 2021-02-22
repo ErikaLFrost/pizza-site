a38d77e0e87780f863bd71cf79ca4bb2b0829717
Erika Loman Frost <erika.loman.frost@sbab.se>
2021-02-22 17:07:02 +0100

    Added images to AboutUs and Merch

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import AboutUsImg from '../images/LPNOm.png';
+import AboutUsImg from '../images/juan-alex2.jpg';
 
 const AboutUsWrapper = styled.div`
-    grid-column: span 5;
+    grid-column: span 6;
     background: 
     linear-gradient(
     rgba(251, 205, 205, 0.52), 
@@ -43,7 +43,7 @@ const AboutUsContent = styled.div`
         p{
             margin-top: 0;
             margin-bottom: .5rem;
-            font-size: 1.5rem;
+            font-size: 1rem;
             padding: 0 10px;
         }
     }
@@ -55,7 +55,11 @@ const AboutUs = () => {
         <AboutUsWrapper>
             <AboutUsContent>
                 <h2>Om oss</h2>
-                <span><p>This is where you finally can get a vegan authentic Neapolitan pizza in Stockholm. It started with three great friends selling Neapolitan pizzas from a bike truck to finally moving in to a small spot in the heart of Södermalm.</p></span>
+                <span><p>Vi är tre vänner som bakar neapolitansk-inspirerad vegansk/vegetarisk pizza utav högsta kvalitet & utan genvägar.
+                Det började med att vi genom vurmandet för god vegansk/vegetarisk mat, & gemensamt pizzanörderi valde att starta upp ett företag. 
+                Från början åkte vi runt och krängde pizza med hjälp av en cykelvagn som seden utvecklades till en moped-bil och nu har vi äntligen flyttat in i en faktisk lokal!
+                Det har varit otroligt nervöst men minst lika kul att äntligen ha fått öppna. Vi är så stolta över att kunna leverera en riktigt god vegansk och vegetarisk pizza i Stockholm till ett rimligt pris. Kom gärna förbi oss och smaka.
+                Varma hälsningar från gänget på nonna!</p></span>
             </AboutUsContent>
         </AboutUsWrapper>)
 }
--- src/components/Merch.js
+++ src/components/Merch.js
@@ -1,13 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
+import MerchImg from '../images/merch2.jpg'
 
 const MerchWrapper = styled.div`
-    
+    grid-column: span 5;
+    grid-column-end: 13;
+    background: 
+    linear-gradient(
+    rgba(251, 205, 205, 0.52), 
+    rgba(251, 205, 205, 0.52)
+    ),
+    url(${MerchImg});
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 0;
+    margin-top: 50px;
+    padding-top: 116.5%;
+    position: relative;
+    @media(max-width: 1000px) {
+        grid-column: span 12;
+    }
+`;
+
+const MerchContent = styled.div`
+    position: absolute;
+    top: 0;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    white-space: pre-line;
+    h2{
+        margin: 0;
+        padding-top: 20px;
+        font-family: 'PlatonickFont';
+        font-size: 50px;
+    }
+    span{
+        text-align: center;
+        p{
+            margin-top: 0;
+            margin-bottom: .5rem;
+            font-size: 1rem;
+            padding: 0 10px;
+        }
+    }
 `;
 
 
 const Merch = () => {
-    return (<MerchWrapper />)
-} 
+    return (
+        <MerchWrapper>
+            <MerchContent>
+            <h2>Merch</h2>
+                <span><p>Här finns det tröjor och grejer kom och köp!</p></span>
+            </MerchContent>
+        </MerchWrapper>)
+}
 
 export default Merch;
