a2cec9dc4a3e847c0c27e3af180c97b391886080
Erika Loman Frost <erika.loman.frost@sbab.se>
2021-03-07 10:16:21 +0100

    Add animation to nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-scroll';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { useTransition, animated } from 'react-spring'
 
 const Hamburger = styled.button`
     position: fixed;
@@ -16,7 +17,6 @@ const Hamburger = styled.button`
     border-left-color: green;
     border-right-color: red;
     cursor: pointer;
-
     :focus {
         outline: none;
     }
@@ -34,16 +34,16 @@ const HamburgerLinesContainer = styled.div`
     }
 `;
 
-const MenuOpen = styled.div`
+const MenuOpen = styled(animated.div)`
     position: fixed;
     top: 0;
-    right: ${props => props.showNav ? '0' : '-100%'};
+    right: 0px;
     width: 50vw;
     max-width: 500px;
     height: 100vh;
     background: #F29699;
     z-index: 1111;
-    transition: right 300ms ease-in;
+    /* transition: right 300ms ease-in; */
     border-left: 2px solid white;
 
     ul {
@@ -62,61 +62,92 @@ const MenuOpen = styled.div`
     }
 `;
 
+const flicker = keyframes`
+  from  { opacity: 0; }
+  0%    { opacity: 1; }
+  3%    { opacity: 0.4; }
+  6%    { opacity: 1; }
+  7%    { opacity: 0.4; }
+  8%    { opacity: 1; }
+  9%    { opacity: 0.4; }
+  10%   { opacity: 1; }
+  89%   { opacity: 1; }
+  90%   { opacity: 0.4; }
+  100%  { opacity: 0.4; }
+  to    { opacity: 1; }
+`;
+
 const StyledLink = styled(Link)`
         color: white;
         text-decoration: none;
-        font-size: 20px;
+        font-size: clamp(1.6rem, 3vw, 3vw);
         cursor: pointer;
+        filter: drop-shadow(0 0 0.5rem rgba(250, 0, 0, 0.76));
+        :hover{
+            animation: ${flicker} 5s linear infinite;
+            color: #e66879;
+            filter: drop-shadow(0 0 0.5rem rgba(243, 180, 182, 0.76));
+        }
 `;
 
 
 const Navigation = () => {
     const [open, setOpen] = useState(false)
 
+    const navigationTransition = useTransition(open, null, {
+        config: { tension: 500, duration: 300, friction: 100 },
+        from: { transform: 'translateX(30%)', opacity: 0 },
+        enter: { transform: 'translateX(0%)', opacity: 1 },
+        leave: { transform: 'translateX(30%)', opacity: 0 },
+    })
+
     return (
         <>
-            <Hamburger onClick={() => setOpen(open ? false : true)}>
+            <Hamburger type="button" onClick={() => setOpen(open ? false : true)}>
                 <HamburgerLinesContainer>
                     <div></div>
                     <div></div>
                     <div></div>
                 </HamburgerLinesContainer>
             </Hamburger>
-            {open}
-            <MenuOpen showNav={open}>
-                <ul>
-                    <li>
-                        <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="topSection" duration={200} smooth={true}>
-                            Top
+            {navigationTransition.map(({ item, props, key }) =>
+                item && (
+                    <MenuOpen style={props} key={key}>
+                        <ul>
+                            <li>
+                                <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="topSection" duration={200} smooth={true}>
+                                    Top
                             </StyledLink>
-                    </li>
-                    <li>
-                        <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="menuSection" duration={200} smooth={true}>
-                            Meny
+                            </li>
+                            <li>
+                                <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="menuSection" duration={200} smooth={true}>
+                                    Meny
                             </StyledLink>
-                    </li>
-                    <li>
-                        <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="visitUsSection" duration={200} smooth={true}>
-                            Öppettider
+                            </li>
+                            <li>
+                                <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="visitUsSection" duration={200} smooth={true}>
+                                    Öppettider
                             </StyledLink>
-                    </li>
-                    <li>
-                        <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="cateringSection" duration={200} smooth={true}>
-                            Catering
+                            </li>
+                            <li>
+                                <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="cateringSection" duration={200} smooth={true}>
+                                    Catering
                             </StyledLink>
-                    </li>
-                    <li>
-                        <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="merchSection" duration={200} smooth={true}>
-                            Merchandise
+                            </li>
+                            <li>
+                                <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="merchSection" duration={200} smooth={true}>
+                                    Merchandise
                             </StyledLink>
-                    </li>
-                    <li>
-                        <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="aboutUsSection" duration={200} smooth={true}>
-                            Om oss
+                            </li>
+                            <li>
+                                <StyledLink activeClass="active" spy={true} onClick={() => setOpen(open ? false : true)} to="aboutUsSection" duration={200} smooth={true}>
+                                    Om oss
                             </StyledLink>
-                    </li>
-                </ul>
-            </MenuOpen>
+                            </li>
+                        </ul>
+                    </MenuOpen>
+                )
+            )}
         </>
     )
 }
