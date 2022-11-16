2808258e7581c6f0787e3ca0283efdc18efdbff7
Erika Loman Frost <erikalfrost@Erikas-MBP.lan>
2022-11-16 18:06:51 +0100

    Fixa copy

--- src/components/VisitUs.js
+++ src/components/VisitUs.js
@@ -106,7 +106,7 @@ const VisitUs = () => {
         <span>
           <h2>Kontakta oss</h2>
           <p>Boka deg eller beställningar? Ring <a href="tel:+4673-234-74-55">073 234 74 55 </a></p>
-          <p>Boka lokalen eller övriga förfrågningar? Mejla oss på <a href = "mailto: lapiccolanonna@gmail.com">lapiccolanonna@gmail.com</a></p>
+          <p>Boka lokalen eller övriga förfrågningar? Mejla <a href = "mailto: lapiccolanonna@gmail.com">lapiccolanonna@gmail.com</a></p>
         </span>
         
       </VisitUsContent>
